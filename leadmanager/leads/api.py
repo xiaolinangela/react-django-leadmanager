2baf61d00d9faefbb190dcfbd82c05e6a273207e
Angela Zhu <xiaolinangela@gmail.com>
2020-04-01 16:15:50 -0700

    setup the django restframewework

---
+++ leadmanager/leads/api.py
@@ -0,0 +1,14 @@
+from leads.models import Lead 
+from rest_framework import viewsets, permissions
+from leads.serializers import LeadSerializer
+
+
+#Lead Viewset
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+
+    serializer_class = LeadSerializer
